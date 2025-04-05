72b9565464a340ecab801d61c3d580af58685091
Kang-Jiho79 <giho2630@gmail.com>
2025-04-05 22:37:30 +0900

    [+] 25.04.05 {3}
    실습 11번

--- Window Programming/test2/test2.cpp
+++ Window Programming/test2/test2.cpp
@@ -3,6 +3,10 @@
 #include <tchar.h>
 #include <stdlib.h>
 #include <time.h>
+#include <math.h>
+
+#define xdistance 250
+#define ydistance 200
 
 HINSTANCE g_hInst;
 LPCTSTR IpszClass = L"Window Class Name";
@@ -43,33 +47,149 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevlnstance, LPSTR lpszCmdPa
 	return Message.wParam;
 }
 
+// 200 x 150 사각형그리기
+void quadrangle(HDC hdc, int x, int y)
+{
+	Rectangle(hdc, x - 100, y - 75, x + 100, y + 75);
+}
 
-int check = 0;
+// 100 x 100 원 그리기
+void circle(HDC hdc, int x, int y)
+{
+	Ellipse(hdc, x - 50, y - 50, x + 50, y + 50);
+}
+// 200 x 100 타원 그리기
+void oval(HDC hdc, int x, int y)
+{
+	Ellipse(hdc, x - 75, y - 50, x + 75, y + 50);
+}
+
+// 100 x 100 모레시계 그리기
+void sandglass(HDC hdc, int x, int y)
+{
+	POINT point[7] = { {x,y},{x - 50,y - 50},{x + 50,y - 50},{x,y},{x + 50,y + 50},{x - 50,y + 50},{x,y} };
+	Polygon(hdc, point, 7);
+}
+// 100 x 100 누워있는 모레시계그리기
+void liedownsandglass(HDC hdc, int x, int y)
+{
+	POINT point[7] = { {x,y},{x - 50,y - 50},{x - 50,y + 50},{x,y},{x + 50,y - 50},{x + 50,y + 50},{x,y} };
+	Polygon(hdc, point, 7);
+}
+
+// 100 x 100 오각형 그리기
+void pentagon(HDC hdc, int x, int y)
+{
+	double pi = 3.14;
+	int r = 60;
+	POINT point[5];
+	for (int i = 0; i < 5; i++) {
+		double angle = -pi / 2 + i * (2 * pi / 5);
+		point[i].x = (x + r * cos(angle));
+		point[i].y = (y + r * sin(angle));
+	}
+	Polygon(hdc, point, 5);
+}
+// 100 x 100 거꾸로된 오각형 그리기
+void reversepentagon(HDC hdc, int x, int y)
+{
+	double pi = 3.14;
+	int r = 60;
+	POINT point[5];
+	for (int i = 0; i < 5; i++) {
+		double angle = pi / 2 + i * (2 * pi / 5);
+		point[i].x = (x + r * cos(angle));
+		point[i].y = (y + r * sin(angle));
+	}
+	Polygon(hdc, point, 5);
+}
+
+// 100 x 100 파이 그리기
+void pie(HDC hdc, int x, int y)
+{
+	Pie(hdc, x - 50, y - 50, x + 50, y + 50, x, y - 50, x + 50, y);
+}
+// 100 x 100 나머지 파이부분 그리기
+void remainpie(HDC hdc, int x, int y)
+{
+	Pie(hdc, x - 50, y - 50, x + 50, y + 50, x + 50, y, x, y - 50);
+}
+
+int present = 4; // 현재 중앙의 그려져있는 도형 0 : 원/ 1 : 모레시계/ 2 : 오각형/ 3 : 파이
+bool pushcheck[5] = { false }; // 눌려있는지 확인
+int turn[4] = { 0,1,2,3 };
+
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam)
 {
 	PAINTSTRUCT ps;
 	HDC hDC;
-	HPEN hDefPen, hSelectPen;
-	HBRUSH hDefBrush, hSelectBrush;
+	HPEN hDefPen;
+	HBRUSH hDefBrush, hRedBrush, hBlueBrush, hGreenBrush, hYellowBrush, hTurquoiseBrush;
 	RECT Now;
-	POINT Triangle[3] = {};
 	GetClientRect(hWnd, &Now);
-	int x = (Now.left+Now.right) / 2 -150, y = (Now.top+Now.bottom) / 2 - 150;
-	int Nowx, Nowy;
+	int x = (Now.left + Now.right) / 2, y = (Now.top + Now.bottom) / 2;
+	POINT point[4] = { {x - xdistance , y}, {x,y - ydistance}, {x + xdistance,y}, { x,y + ydistance } };
 	switch (iMessage) {
 
-	case WM_CHAR:
+	case WM_KEYDOWN:
 	{
-		if (wParam == 't')
-			check = 1;
-		else if (wParam == 'r')
-			check = 2;
-		else if (wParam == 'l')
-			check = 3;
-		else if (wParam == 'q') {
+		if (wParam == 'Q') {
 			PostQuitMessage(0);
-			return 0;
+			break;
+		}
+		else if (wParam == 'C') {
+			pushcheck[0] = true;
+			present = 0;
+		}
+		else if (wParam == 'S') {
+			pushcheck[1] = true;
+			present = 1;
+		}
+		else if (wParam == 'P') {
+			pushcheck[2] = true;
+			present = 2;
+		}
+		else if (wParam == 'E') {
+			pushcheck[3] = true;
+			present = 3;
+		}
+		else if (wParam == VK_LEFT) {
+			for (int i = 0; i < 4; i++) {
+				if (turn[i] == 0)
+					turn[i] = 3;
+				else
+					turn[i]--;
+				if (turn[i] == 0)
+					present = i;
+			}
+		}
+		else if (wParam == VK_RIGHT) {
+			for (int i = 0; i < 4; i++) {
+				if (turn[i] == 3)
+					turn[i] = 0;
+				else
+					turn[i]++;
+				if (turn[i] == 0)
+					present = i;
+			}
+		}
+		InvalidateRect(hWnd, NULL, TRUE);
+	}
+	break;
+	case WM_KEYUP:
+	{
+		if (wParam == 'C') {
+			pushcheck[0] = false;
+		}
+		else if (wParam == 'S') {
+			pushcheck[1] = false;
+		}
+		else if (wParam == 'P') {
+			pushcheck[2] = false;
+		}
+		else if (wParam == 'E') {
+			pushcheck[3] = false;
 		}
 		InvalidateRect(hWnd, NULL, TRUE);
 	}
@@ -77,110 +197,54 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam)
 	case WM_PAINT:
 	{
 		hDC = BeginPaint(hWnd, &ps);
-		Rectangle(hDC, x, y, x + 300, y + 300);
-		if (check == 1) {
-			hSelectPen = CreatePen(PS_SOLID, 1, RGB(255, 0, 0));
-			hSelectBrush = CreateSolidBrush(RGB(255, 0, 0));
-			hDefPen = (HPEN)SelectObject(hDC, hSelectPen);
-			hDefBrush = (HBRUSH)SelectObject(hDC, hSelectBrush);
-			for (int i = 0; i < 11; i++) {
-				Nowx = x + (25 * (i + 1));
-				Nowy = y;
-				Triangle[0] = { Nowx, (Nowy -7) };
-				Triangle[1] = { Nowx + 10,Nowy +4 };
-				Triangle[2] = { Nowx - 10,Nowy +4 };
-				Polygon(hDC, Triangle, 3);
-			}
-			for (int i = 0; i < 11; i++) {
-				Nowx = x + (25 * (i + 1));
-				Nowy = y + 300;
-				Triangle[0] = { Nowx, (Nowy - 7) };
-				Triangle[1] = { Nowx + 10,Nowy + 4 };
-				Triangle[2] = { Nowx - 10,Nowy + 4 };
-				Polygon(hDC, Triangle, 3);
-			}
-			for (int i = 0; i < 11; i++) {
-				Nowx = x;
-				Nowy = y + (25 * (i + 1));
-				Triangle[0] = { Nowx, (Nowy - 7) };
-				Triangle[1] = { Nowx + 10,Nowy + 4 };
-				Triangle[2] = { Nowx - 10,Nowy + 4 };
-				Polygon(hDC, Triangle, 3);
-			}
-			for (int i = 0; i < 11; i++) {
-				Nowx = x + 300;
-				Nowy = y + (25 * (i + 1));
-				Triangle[0] = { Nowx, (Nowy - 7) };
-				Triangle[1] = { Nowx + 10,Nowy + 4 };
-				Triangle[2] = { Nowx - 10,Nowy + 4 };
-				Polygon(hDC, Triangle, 3);
-			}
-			SelectObject(hDC, hDefPen);
-			SelectObject(hDC, hDefBrush);
-			DeleteObject(hSelectPen);
-			DeleteObject(hSelectBrush);
-		}
+		hRedBrush = CreateSolidBrush(RGB(255, 0, 0));
+		hBlueBrush = CreateSolidBrush(RGB(0, 0, 255));
+		hGreenBrush = CreateSolidBrush(RGB(0, 255, 0));
+		hYellowBrush = CreateSolidBrush(RGB(255, 255, 0));
+		hTurquoiseBrush = CreateSolidBrush(RGB(0, 255, 255));
+		quadrangle(hDC, x, y);
+		if (pushcheck[0])
+			hDefBrush = (HBRUSH)SelectObject(hDC, hTurquoiseBrush);
+		else
+			hDefBrush = (HBRUSH)SelectObject(hDC, hBlueBrush);
+		circle(hDC, point[turn[0]].x, point[turn[0]].y);
+		if (present == 0)
+			oval(hDC, x, y);
+		SelectObject(hDC, hDefBrush);
 
-		else if (check == 2) {
-			hSelectPen = CreatePen(PS_SOLID, 1, RGB(0, 255, 0));
-			hSelectBrush = CreateSolidBrush(RGB(0, 255, 0));
-			hDefPen = (HPEN)SelectObject(hDC, hSelectPen);
-			hDefBrush = (HBRUSH)SelectObject(hDC, hSelectBrush);
-			for (int i = 0; i < 11; i++) {
-				Nowx = x + (25 * (i + 1));
-				Nowy = y;
-				Rectangle(hDC, Nowx - 10, Nowy - 10, Nowx + 10, Nowy + 10);
-			}
-			for (int i = 0; i < 11; i++) {
-				Nowx = x + (25 * (i + 1));
-				Nowy = y + 300;
-				Rectangle(hDC, Nowx - 10, Nowy - 10, Nowx + 10, Nowy + 10);
-			}
-			for (int i = 0; i < 11; i++) {
-				Nowx = x;
-				Nowy = y + (25 * (i + 1));
-				Rectangle(hDC, Nowx - 10, Nowy - 10, Nowx + 10, Nowy + 10);
-			}
-			for (int i = 0; i < 11; i++) {
-				Nowx = x + 300;
-				Nowy = y + (25 * (i + 1));
-				Rectangle(hDC, Nowx - 10, Nowy - 10, Nowx + 10, Nowy + 10);
-			}
-			SelectObject(hDC, hDefPen);
-			SelectObject(hDC, hDefBrush);
-			DeleteObject(hSelectPen);
-			DeleteObject(hSelectBrush);
-		}
-		else if (check == 3) {
-			hSelectPen = CreatePen(PS_SOLID, 5, RGB(0, 0, 255));
-			hDefPen = (HPEN)SelectObject(hDC, hSelectPen);
-			for (int i = 0; i < 11; i++) {
-				Nowx = x + (25 * (i + 1));
-				Nowy = y;
-				MoveToEx(hDC, Nowx-5, Nowy, NULL);
-				LineTo(hDC, Nowx+5, Nowy);
-			}
-			for (int i = 0; i < 11; i++) {
-				Nowx = x + (25 * (i + 1));
-				Nowy = y + 300;
-				MoveToEx(hDC, Nowx - 5, Nowy, NULL);
-				LineTo(hDC, Nowx + 5, Nowy);
-			}
-			for (int i = 0; i < 11; i++) {
-				Nowx = x;
-				Nowy = y + (25 * (i + 1));
-				MoveToEx(hDC, Nowx, Nowy-5, NULL);
-				LineTo(hDC, Nowx, Nowy+5);
-			}
-			for (int i = 0; i < 11; i++) {
-				Nowx = x + 300;
-				Nowy = y + (25 * (i + 1));
-				MoveToEx(hDC, Nowx, Nowy - 5, NULL);
-				LineTo(hDC, Nowx, Nowy + 5);
-			}
-			SelectObject(hDC, hDefPen);
-			DeleteObject(hSelectPen);
-		}
+		if (pushcheck[1])
+			hDefBrush = (HBRUSH)SelectObject(hDC, hTurquoiseBrush);
+		else
+			hDefBrush = (HBRUSH)SelectObject(hDC, hRedBrush);
+		sandglass(hDC, point[turn[1]].x, point[turn[1]].y);
+		if (present == 1)
+			liedownsandglass(hDC, x, y);
+		SelectObject(hDC, hDefBrush);
+
+
+		if (pushcheck[2])
+			hDefBrush = (HBRUSH)SelectObject(hDC, hTurquoiseBrush);
+		else
+			hDefBrush = (HBRUSH)SelectObject(hDC, hYellowBrush);
+		pentagon(hDC, point[turn[2]].x, point[turn[2]].y);
+		if (present == 2)
+			reversepentagon(hDC, x, y);
+		SelectObject(hDC, hDefBrush);
+
+		if (pushcheck[3])
+			hDefBrush = (HBRUSH)SelectObject(hDC, hTurquoiseBrush);
+		else
+			hDefBrush = (HBRUSH)SelectObject(hDC, hGreenBrush);
+		pie(hDC, point[turn[3]].x, point[turn[3]].y);
+		if (present == 3)
+			remainpie(hDC, x, y);
+		SelectObject(hDC, hDefBrush);
+
+		DeleteObject(hRedBrush);
+		DeleteObject(hBlueBrush);
+		DeleteObject(hGreenBrush);
+		DeleteObject(hYellowBrush);
+		DeleteObject(hTurquoiseBrush);
 		EndPaint(hWnd, &ps);
 	}
 	break;
