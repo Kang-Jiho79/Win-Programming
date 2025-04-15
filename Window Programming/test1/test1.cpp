f37470f13becdec6837524a1712740c0d53345f4
Kang-Jiho79 <giho2630@gmail.com>
2025-04-15 14:56:38 +0900

    [+] 25.04.15 {1}
    실습 3-1

--- Window Programming/test1/test1.cpp
+++ Window Programming/test1/test1.cpp
@@ -36,7 +36,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevlnstance, LPSTR lpszCmdPa
 	WndClass.hIconSm = LoadIcon(NULL, IDI_APPLICATION);		//작은 아이콘 (보통 hIcon과 같은걸 사용)
 	RegisterClassEx(&WndClass);
 
-	hWnd = CreateWindow(IpszClass, IpszWindowName, WS_OVERLAPPEDWINDOW, 0, 0, 1100, 1100, NULL, (HMENU)NULL, hInstance, NULL);	//윈도우 만들기 함수
+	hWnd = CreateWindow(IpszClass, IpszWindowName, WS_OVERLAPPEDWINDOW, 0, 0, 800, 600, NULL, (HMENU)NULL, hInstance, NULL);	//윈도우 만들기 함수
 
 	ShowWindow(hWnd, nCMdShow);	//	윈도우 띄우기
 	UpdateWindow(hWnd);		// 윈도우 업데이트
@@ -49,102 +49,31 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevlnstance, LPSTR lpszCmdPa
 }
 
 struct Player {
-	int x, y;
+	int x, y, speed, size;
+	int movecheck;
+	bool width, length;
 };
 
-struct Cell {
-	bool check = false;
-	int wordnumber = 0;
-};
+
 
 Player player;
-Cell table[tablecount][tablecount];
-TCHAR words[5][6] = { L"APPLE", L"WORLD", L"CLASS", L"TABLE", L"STUDY" };
-int r;
-	
-
 
 // 게임 기초 세팅
 void settinggame()
 {
-	srand((unsigned)time(0));
-	r = rand() % 5;
-	for (int i =0;i<tablecount; i++)
-		for (int j = 0; j < tablecount; j++) {
-			table[i][j].check = false;
-			table[i][j].wordnumber = 0;
-		}
-	for (int i = 0; i < 5; i++) {
-		if (i == r) {
-			table[1][8 + i].wordnumber = 0;
-			table[1][8 + i].check = false;
-		}
-		else {
-			table[1][8 + i].wordnumber = words[r][i] - 'A';
-			table[1][8 + i].check = true;
-		}
-	}
-	for (int i = 3; i < tablecount-1; ++i) {
-		for (int j = 1; j < tablecount-1; ++j) {
-			int r = rand() % 10;
-			if (r == 0) {
-				table[i][j].wordnumber =rand() % 26;
-				table[i][j].check = true;
-			}
-		}
-	}
-	player.x = 10;
-	player.y = 10;
+	player.x = 20;
+	player.y = 20;
+	player.size = 20;
+	player.speed = 5;
+	player.movecheck = 0;
+	player.width = true;
+	player.length = true;
 }
 
-
-
-
-void drawtable(HDC hdc)
-{
-	for (int i = 0; i < tablecount; i++) {
-		for (int j = 0; j < tablecount; j++) {
-			if (table[i][j].check == true) {
-				TCHAR ch = 'A' + table[i][j].wordnumber;
-				TextOut(hdc, ((j * cellsize) + cellsize / 2)-5, ((i * cellsize) + cellsize / 2)-5, &ch , 1);
-			}
-		}
-	}
-	for (int i = 0; i <= tablecount; i++) {
-		MoveToEx(hdc, i * cellsize, 0, NULL);
-		LineTo(hdc, i * cellsize, cellsize * tablecount);
-		MoveToEx(hdc, 0, i * cellsize, NULL);
-		LineTo(hdc, cellsize * tablecount, i * cellsize);
-	}
-}
-
-int wordnumadd(int a, int b)
-{
-	return (a + b > 25 ? a + b - 25 : a + b+1);
-}
 
 void MovePlayer(int dx, int dy) {
-	int nx = player.x + dx;
-	int ny = player.y + dy;
-	if (nx < 0 || ny < 0 || nx >= tablecount || ny >= tablecount)
-		return;
-	if (table[ny][nx].check)
-		if(nx + dx < 0 || ny + dy < 0 || nx + dx >= tablecount || ny + dy >= tablecount)
-			return;
-	player.x = nx;
-	player.y = ny;
-
-	if (table[ny][nx].check) {
-		if (table[ny + dy][nx + dx].check) {
-			table[ny + dy][nx + dx].wordnumber = wordnumadd(table[ny + dy][nx + dx].wordnumber, table[ny][nx].wordnumber);
-		}
-		else {
-			table[ny + dy][nx + dx].wordnumber = table[ny][nx].wordnumber;
-			table[ny + dy][nx + dx].check = true;
-		}
-		table[ny][nx].check = false;
-		table[ny][nx].wordnumber = 0;
-	}
+	player.x += dx;
+	player.y += dy;
 }
 
 
@@ -164,58 +93,92 @@ void drawshape(HDC hdc, int x, int y)
 	DeleteObject(brush);
 }
 
-bool endcheck()
+LRESULT CALLBACK WndProc(HWND hWnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
 {
-	bool endchecking = true;
-	for (int i = 0; i < 5; i++) {
-		if (table[1][8 + i].wordnumber != words[r][i] - 'A' && !table[1][8+i].check)
-			endchecking = false;
-	}
-	return endchecking;
-}
-
-LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam)
-{
-	PAINTSTRUCT ps;
 	HDC hDC;
-	HPEN hDefPen;
-	HBRUSH hDefBrush, hRedBrush, hBlueBrush, hGreenBrush, hYellowBrush, hTurquoiseBrush;
-	RECT Now;
-	switch (iMessage) {
+	PAINTSTRUCT ps;
+	static RECT rectView;
+	switch (iMsg)
+	{
 	case WM_CREATE:
+		GetClientRect(hWnd, &rectView);
+		SetTimer(hWnd, 1, 1000 * player.speed, NULL);
 		settinggame();
 		break;
 	case WM_PAINT:
-	{
-		PAINTSTRUCT ps;
-		HDC hdc = BeginPaint(hWnd, &ps);
-		drawtable(hdc);
-		drawshape(hdc, player.x, player.y);
-		if (endcheck())
-			TextOut(hdc, 0, cellsize * tablecount+10, L"단어 완성!", _tcslen(L"단어 완성!"));
-		else
-			TextOut(hdc, 0, cellsize * tablecount+10, words[r], _tcslen(words[r]));
+		hDC = BeginPaint(hWnd, &ps);
+		circle(hDC, player.x, player.y,player.size);
 		EndPaint(hWnd, &ps);
-	}
-	break;
-	case WM_KEYDOWN:
-		if (!endcheck()) {
-			if (wParam == VK_UP) MovePlayer(0, -1);
-			else if (wParam == VK_LEFT) MovePlayer(-1, 0);
-			else if (wParam == VK_DOWN) MovePlayer(0, 1);
-			else if (wParam == VK_RIGHT) MovePlayer(1, 0);
+		break;
+	case WM_CHAR:
+		if (wParam == 'h')
+			player.movecheck = 1;
+		else if (wParam == 'v')
+			player.movecheck = 2;
+		else if (wParam == 'p')
+			player.movecheck = 0;
+		else if (wParam == '-') {
+			if (player.speed > 1) {
+				player.speed--;
+			}
 		}
-		if (wParam == 'Q')
+		else if (wParam == '+') {
+			if (player.speed < 10) {
+				player.speed++;
+			}
+		}
+		else if (wParam == 'q')
 			PostQuitMessage(0);
-		else if (wParam == 'S')
-			settinggame();
+		InvalidateRect(hWnd, NULL, TRUE);
+		break;
+	case WM_LBUTTONDOWN:
+		player.x = LOWORD(lParam);
+		player.y = HIWORD(lParam);
+		InvalidateRect(hWnd, NULL, TRUE);
+		break;
+	case WM_TIMER:
+		if (player.movecheck == 1) {
+			if (player.x + player.size / 2 > 800) {
+				player.width = false;
+				if (player.y + player.size > 600)
+					player.y = 20;
+				else 
+					player.y += cellsize;
+			}
+			else if (player.x - player.size / 2 < 0) {
+				player.width = true;
+				if (player.y + player.size > 600)
+					player.y = 20;
+				else
+					player.y += cellsize;
+			}
+			if (player.width) { MovePlayer(player.speed, 0); }
+			else { MovePlayer(-player.speed, 0); }
+		}
+		else if (player.movecheck == 2) {
+			if (player.y + player.size / 2 > 600) {
+				player.length = false;
+				if (player.x + player.size > 800)
+					player.x = 20;
+				else
+					player.x += cellsize;
+			}
+			else if (player.y - player.size / 2 < 0) {
+				player.length = true;
+				if (player.x + player.size > 800)
+					player.x = 20;
+				else
+					player.x += cellsize;
+			}
+			if (player.length) { MovePlayer(0, player.speed); }
+			else { MovePlayer(0, -player.speed); }
+		}
 		InvalidateRect(hWnd, NULL, TRUE);
 		break;
 	case WM_DESTROY:
+		KillTimer(hWnd, 1);
 		PostQuitMessage(0);
 		break;
-	default:
-		return DefWindowProc(hWnd, iMessage, wParam, lParam);
 	}
-	return 0;
+	return DefWindowProc(hWnd, iMsg, wParam, lParam);
 }
